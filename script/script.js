de43760d9bee0b69c3bc99ccadb77bdcdf6960f3
khalilnazari <khalil.deve@gmail.com>
2022-02-17 17:37:48 +0800

    home, about, services and projects styling is written. Javascript for navbar is written and Swipperjs is added

---
+++ script/script.js
@@ -0,0 +1,23 @@
+let navbar = document.querySelector('.header .navbar');
+let searchForm = document.querySelector('.header .search-form');
+let loginForm = document.querySelector('.header .login-form');
+let contactInfo = document.querySelector('.contact-info');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   searchForm.classList.remove('active');
+   loginForm.classList.remove('active');
+};
+
+
+
+// Swipper slider
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    grabCursor:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+ });
+ 
