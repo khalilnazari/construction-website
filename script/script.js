fbb3e8906a2382ebb7e4a284a53c169d8e318513
khalilnazari <khalil.deve@gmail.com>
2022-02-18 16:16:40 +0800

    css style and script is added for the rest of the page.

--- script/script.js
+++ script/script.js
@@ -3,15 +3,40 @@ let searchForm = document.querySelector('.header .search-form');
 let loginForm = document.querySelector('.header .login-form');
 let contactInfo = document.querySelector('.contact-info');
 
+
+// navbar toggle
 document.querySelector('#menu-btn').onclick = () =>{
    navbar.classList.toggle('active');
    searchForm.classList.remove('active');
    loginForm.classList.remove('active');
 };
 
+// info toggle 
+document.querySelector('#info-btn').onclick = () =>{
+  contactInfo.classList.add('active');
+}
+document.querySelector('#close-contact-info').onclick = () =>{
+  contactInfo.classList.remove('active');
+}
 
 
-// Swipper slider
+// Search 
+document.querySelector('#search-btn').onclick = () =>{
+  searchForm.classList.toggle('active');
+  navbar.classList.remove('active');
+  loginForm.classList.remove('active');
+};
+
+
+// login form toggle 
+document.querySelector('#login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
+  navbar.classList.remove('active');
+  searchForm.classList.remove('active'); 
+};
+
+
+// home-slider
 var swiper = new Swiper(".home-slider", {
     loop:true,
     grabCursor:true,
@@ -21,3 +46,44 @@ var swiper = new Swiper(".home-slider", {
     },
  });
  
+
+
+// Review Slider 
+var swiper = new Swiper(".reviews-slider", {
+  loop:true,
+  grabCursor:true,
+  spaceBetween: 20,
+  breakpoints: {
+     640: {
+       slidesPerView: 1,
+     },
+     768: {
+       slidesPerView: 2,
+     },
+     991: {
+       slidesPerView: 3,
+     },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+
+// blog slider
+var swiper = new Swiper(".blogs-slider", {
+  loop:true,
+  grabCursor:true,
+  spaceBetween: 20,
+  breakpoints: {
+     640: {
+       slidesPerView: 1,
+     },
+     768: {
+       slidesPerView: 2,
+     },
+     991: {
+       slidesPerView: 3,
+     },
+  },
+});
